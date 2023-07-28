5f0b1bb140dac575bbd6f6b45c69c446b0f0fc74
Lord Basil <126736516+Drlordbasil@users.noreply.github.com>
2023-07-27 23:17:11 -0400

    Fix syntax error

--- main.py
+++ main.py
@@ -8,20 +8,24 @@ consumer_secret = 'your_consumer_secret'
 access_token = 'your_access_token'
 access_token_secret = 'your_access_token_secret'
 
+
 def authenticate():
     # Authenticating with Twitter API
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
     return tweepy.API(auth)
 
+
 def get_tweets(api, query, count=100):
     # Fetching tweets based on query
     try:
         fetched_tweets = api.search(q=query, count=count)
-        tweets = [{'text': tweet.text, 'sentiment': get_tweet_sentiment(tweet.text)} for tweet in fetched_tweets]
+        tweets = [{'text': tweet.text, 'sentiment': get_tweet_sentiment(
+            tweet.text)} for tweet in fetched_tweets]
         return tweets
     except tweepy.TweepError as e:
         print("Error : " + str(e))
+
 
 def get_tweet_sentiment(tweet):
     # Analyzing sentiment using TextBlob
@@ -34,23 +38,28 @@ def get_tweet_sentiment(tweet):
     else:
         return 'negative'
 
+
 def plot_sentiment_pie_chart(sentiment_counts):
     # Plotting pie chart of sentiment distribution
     labels = list(sentiment_counts.keys())
     sizes = list(sentiment_counts.values())
     colors = ['green', 'yellow', 'red']
-    plt.pie(sizes, labels=labels, colors=colors, autopct='%1.1f%%', startangle=140)
+    plt.pie(sizes, labels=labels, colors=colors,
+            autopct='%1.1f%%', startangle=140)
     plt.axis('equal')
     plt.title("Sentiment Analysis: Social Media Posts")
     plt.show()
+
 
 def preprocess_tweets(tweets):
     # Preprocessing tweets
     cleaned_tweets = []
     for tweet in tweets:
-        cleaned_tweet = ''.join(char for char in tweet['text'] if char.isalnum() or char.isspace())
+        cleaned_tweet = ''.join(
+            char for char in tweet['text'] if char.isalnum() or char.isspace())
         cleaned_tweets.append(cleaned_tweet)
     return cleaned_tweets
+
 
 def analyze_sentiment(cleaned_tweets):
     # Sentiment analysis
@@ -59,6 +68,7 @@ def analyze_sentiment(cleaned_tweets):
         sentiment = get_tweet_sentiment(tweet)
         sentiment_counts[sentiment] += 1
     return sentiment_counts
+
 
 def sentiment_analysis(query):
     # Authenticating with Twitter API
@@ -76,7 +86,9 @@ def sentiment_analysis(query):
     # Visualization
     plot_sentiment_pie_chart(sentiment_counts)
 
+
 # Main driver function
 if __name__ == "__main__":
-    query = input("Enter a keyword to perform sentiment analysis on social media posts: ")
+    query = input(
+        "Enter a keyword to perform sentiment analysis on social media posts: ")
     sentiment_analysis(query)
